a66eec20d0a9298b2f826591c5062f7bb0e82f4a
Your Name <you@example.com>
2023-11-05 17:30:17 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,15 @@ import InputField from "./Component/InputField";
 import { Todo } from "./model";
 import TodoList from "./Component/TodoList";
 
-
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string | number>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleAdd= (e: React.FormEvent)=> {
+  const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone:false}]);
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
       setTodo("");
     }
   };
@@ -25,9 +24,6 @@ const App: React.FC = () => {
       <span className="heading">To-Do List</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
-      {/* {todos.map((t)=> (
-        <li>{t.todo}</li>
-      ))} */}
     </div>
   );
 };
